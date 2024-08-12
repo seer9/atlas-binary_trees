6e27f8a96183cf796ceae2ed08f1613639eba3d7
Clay <rpctropic13@gmail.com>
2024-08-12 22:37:00 +0000

    the null parent didnt need the space

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_insert_left - inserts a new node as the left child of another node
+ * @parent: pointer to the parent node
+ * @value: value to store in the node
+ *
+ * Return: pointer to the created node
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
   /* node to be added */
