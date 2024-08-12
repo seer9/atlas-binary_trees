2bf5f8314bcc61a1d816fa0a8c270e6d6a9a668a
Clay <rpctropic13@gmail.com>
2024-08-12 23:44:01 +0000

    doc check

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_preorder - preorder traversal of a binary tree
 
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to the function to call for each node
+ */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
   if (tree == NULL)
