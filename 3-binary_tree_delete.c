6a7916f7d0e79bb5110954e8967ccf865032f6bb
Clay <rpctropic13@gmail.com>
2024-08-12 23:18:53 +0000

    double edge

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,15 +2,15 @@
 
 /**
  * binary_tree_delete - deletes a node of a binary tree
- * 
+ *
  * @tree: pointer to the node to delete
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
+  if (tree == NULL)
+    return;
 
-   binary_tree_delete(tree->left);
-   binary_tree_delete(tree->right);
-   free(tree);
+  binary_tree_delete(tree->left);
+  binary_tree_delete(tree->right);
+  free(tree);
 }
