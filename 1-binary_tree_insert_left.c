205d1fe67cb091dc63d146308eeef5ffaed1ad12
Clay <rpctropic13@gmail.com>
2024-08-12 22:19:57 +0000

    tripped up by parent being null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,11 +2,20 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+
   /* memory for the new node */
   binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
+  if (parent == NULL)
+    {
+      return (NULL);
+    }
+
   /* if failed, return NULL */
   if (new_node == NULL)
+    return (NULL);
+
+  if (parent == NULL)
     return (NULL);
 
   /* Initialize the new node */
