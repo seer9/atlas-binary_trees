227427088d79c6a489729e3448e1a3071f02ba45
Clay <rpctropic13@gmail.com>
2024-08-13 00:33:47 +0000

    getting better

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - creates an array of nodes
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: pointer to the array of nodes
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+  if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+    return (0);
+
+return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+}
