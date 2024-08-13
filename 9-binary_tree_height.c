8bf2e7b0bc202b6d8c88f5594876d54f8b82c13b
Clay <rpctropic13@gmail.com>
2024-08-13 00:23:18 +0000

    logic check

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure
+ *
+ * Return: height of the tree starting from 0
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
     size_t left, right;
