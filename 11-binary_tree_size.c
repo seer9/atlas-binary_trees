fc59a7a536f4e7fe754381ab79d2a73fcd51a797
Clay <rpctropic13@gmail.com>
2024-08-13 00:24:52 +0000

    rechecked

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure the size
+ * Return: Size of the tree starting from 0
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
     size_t size = 0;
